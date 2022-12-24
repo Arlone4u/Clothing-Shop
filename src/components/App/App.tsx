9ff7cc04debe1bba197be2566f212a9c0fd60942
Arlone4u <arlochristian.verallo.21@usjr.edu.ph>
2022-12-24 11:54:51 +0800

    Adding Wishlist and Checkout operation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,24 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { LinksWrapper, TitleWrapper, Wrapper } from "./App.styled";
 
+import { Wishlist } from "../Wishlist";
 import { Cart } from "../Cart";
 import { Products } from "../Products";
-import { ClothingShopContext } from "../../useContext";
 import { useReducer } from "react";
-import { add, initialState, remove, shopReducer, update } from "../../useReducer";
 import { Product } from "../../models";
+import {
+  shopReducer,
+  initialState,
+  add,
+  remove,
+  update,
+  addQty,
+  erase,
+  save,
+  totalItems,
+} from "../Reducer";
+import { ClothingShopContext } from "../Context";
+import { Checkout } from "../Checkout";
 
 export const App = () => {
   const [state, dispatch] = useReducer(shopReducer, initialState);
@@ -18,7 +30,7 @@ export const App = () => {
     dispatch(add(updatedCart));
   };
 
-  const removeItem = (product: Product) => {
+  const removeFromCart = (product: Product) => {
     const updatedCart = state.products.filter(
       (currentProduct: Product) => currentProduct.name !== product.name
     );
@@ -29,16 +41,50 @@ export const App = () => {
 
   const updatePrice = (products: [] = []) => {
     let total = 0;
-    products.forEach((product: { price: number; }) => (total = total + product.price));
+    let items = 0;
+    products.forEach(
+      (product: { price: number; quantity: number }) =>
+        (total = total + product.price * product.quantity , items = items + product.quantity)
+    );
 
     dispatch(update(total));
+    dispatch(totalItems(items));
   };
+
+  const addToWishlist = (product: Product) => {
+    const updatedCart = state.saved.concat(product);
+    dispatch(save(updatedCart));
+  };
+
+  const removeFromWishlist = (product: Product) => {
+    const updatedCart = state.saved.filter(
+      (currentProduct: Product) => currentProduct.name !== product.name
+    );
+    dispatch(erase(updatedCart));
+  };
+
+
+  const updateCart = (product: Product, quantity: number) => {
+    const updatedCart = state.products.map((items: { name: string }) =>
+      items.name === product.name ? { ...items, quantity: quantity } : items
+    );
+    dispatch(addQty(updatedCart));
+
+    updatePrice(updatedCart);
+  };
+
   const value = {
+    totalitems: state.totalitems,
     total: state.total,
     products: state.products,
+    saved: state.saved,
     addToCart,
-    removeItem
-  }
+    removeFromCart,
+    addToWishlist,
+    removeFromWishlist,
+    updatePrice,
+    updateCart,
+  };
   return (
     <ClothingShopContext.Provider value={value}>
       <Wrapper>
@@ -47,11 +93,15 @@ export const App = () => {
         </TitleWrapper>
         <LinksWrapper>
           <Link to="/">Home</Link>
+          <Link to="/wishlist">Wishlist</Link>
           <Link to="/cart">Cart</Link>
+          <Link to="/checkout">Checkout</Link>
         </LinksWrapper>
         <Routes>
           <Route path="/" element={<Products />} />
+          <Route path="/wishlist" element={<Wishlist />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </Wrapper>
     </ClothingShopContext.Provider>
